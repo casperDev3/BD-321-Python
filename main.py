e55c8088f9d625ffae04a384e0c000543fe9b246
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-07-03 19:14:57 +0300

    add bs template lesson

--- main.py
+++ main.py
@@ -1,44 +1,4 @@
 if __name__ == "__main__":
-    name = "John"
-    fatherName = "Patrick"
-    surname = "doe"
-
-    user_data = {"name": name, "fathersName": fatherName, "surname": surname,
-                 "pib": f"{surname.title()} {name[0].upper()}. {fatherName[0].upper()}.", 'age': 26}
-
-    # reverse
-    # print(fatherName[::-1])
-    # print(arr[::-1])
-
-    # TUPLE
-    arr_names = ("John", "Jerry", "Simmon")
-
-
-
-    #SET
-    arr_surnames = {"Doe", "Lee", "Chan"}
-    arr_surnames.add('Jane')
-    print(arr_surnames)
-    arr_surnames.remove('Lee')
-    print(arr_surnames)
-
-    print(user_data['pib'])
-
-
-    # # Primitive Date
-    # a = 1
-    # b = a
-    # b += 1
-    #
-    # print(a, b)
-    #
-    # # Complex Data
-    # c = [1]
-    # d = c.copy()
-    # d[0] += 1
-    #
-    # print(c, d)
-
     users = [
         {
             "name": "John",
@@ -57,7 +17,7 @@ if __name__ == "__main__":
             "age": 31
         }
     ]
+    temp_age = users[1]['age']
 
-    
 
 
