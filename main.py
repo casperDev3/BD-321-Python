ee0cbddc0279453e4bbcfa2e93a5f2a22a4e3a25
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-07-03 18:38:37 +0300

    template formatt text (pib)

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 if __name__ == "__main__":
-    print("Hello World!")
+    name = "John"
+    fatherName = "Patrick"
+    surname = "doe"
+
+    user_data = {"name": name, "fathersName": fatherName, "surname": surname,
+                 "pib": f"{surname.title()} {name[0].upper()}. {fatherName[0].upper()}.", 'age': 26}
+
+    # reverse
+    # print(fatherName[::-1])
+    # print(arr[::-1])
+
+    # TUPLE
+    arr_names = ("John", "Jerry", "Simmon")
+
+
+
+    #SET
+    arr_surnames = {"Doe", "Lee", "Chan"}
+    arr_surnames.add('Jane')
+    print(arr_surnames)
+    arr_surnames.remove('Lee')
+    print(arr_surnames)
+
+    print(user_data['pib'])
