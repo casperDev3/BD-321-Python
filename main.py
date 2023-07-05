421d18338574506d23752797cd91737ad55dc526
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-07-05 19:27:25 +0300

    add lesson

--- main.py
+++ main.py
@@ -1,41 +1,8 @@
 if __name__ == "__main__":
-    # users = [
-    #     {
-    #         "name": "John",
-    #         "age": 31
-    #     },
-    #     {
-    #         "name": "Jane",
-    #         "age": 27
-    #     },
-    #     {
-    #         "name": "Jack",
-    #         "age": 15
-    #     },
-    #     {
-    #         "name": "John",
-    #         "age": 31
-    #     }
-    # ]
-    # temp_age = 6
-    # temp_name = users[1]['name']
-    # print(temp_age, temp_name)
-    print("___ START PROGRAM ___ \n\n")
-    system_metrics = input(f"Please choice system metrics:\n"
-                           f"c - Celsius\n"
-                           f"f - Farengei\n"
-                           f"k - Kalvin\n"
-                           f"Please choice: ")
-    temperature_value = int(input(f"Please input temperatute value:  "))
-
-    # conditional temp
-    if system_metrics.lower() == "c":
-        print(f"INPUT VALUE: {temperature_value} C\n\n")
-        print(f"Kalvin: {round(temperature_value + 273.75, 2)}")
-        print(f"Farengeit: {round((temperature_value * (9 / 5)) + 32, 2)}")
-    elif system_metrics.lower() == "f":
-        print(f"INPUT VALUE: {temperature_value} F\n\n")
-        print(f"Kalvin: {((temperature_value - 32) + 273.75) / (9 / 5)}")
-        print(f"Celsium: {(temperature_value / (9 / 5)) - 32}")
+    temp = int(input("Vvedit temperaturu na dvori: "))
+    if 10 < temp < 20:
+        print("TEST")
+    elif temp > 20:
+        print("Harashe")
     else:
-        print("CALC SELF!")
+        print("Cholodno")
