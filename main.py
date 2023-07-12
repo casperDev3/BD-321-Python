46887231622fcb2d137be57fdde32a031954351b
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-07-12 17:04:35 +0300

    start comit

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
 import json
 if __name__ == "__main__":
-    with open('products.json', "r") as file:
-        products = json.load(file)
-           
-
-    total_price = 0
-    for product in products:
-        total_price += product['price']
-
-    print(round(total_price, 2))
-
+    print("Hello world!")
