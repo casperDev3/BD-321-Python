1a3a5454e3ff923808f6c8c8b24ddeaa6dd7d2da
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-07-17 19:57:50 +0300

    add cesar code

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import json
 import transform_temp as tt
 
-if __name__ == "__main__":
-    system_metrics = input(f"Please choice system metrics:\n"
-                           f"c - Celsius\n"
-                           f"f - Farengei\n"
-                           f"k - Kalvin\n"
-                           f"Please choice: ")
-    temperature_value = int(input(f"Please input temperatute value:  "))
 
-    # conditional temp
-    if system_metrics.lower() == "c":
-        tt.tranformCelsius(temperature_value)
-    elif system_metrics.lower() == "f":
-        tt.tranformFarengeit(temperature_value)
+def isPolindrom(word):
+    reverse_word = word[::-1]
+    if reverse_word == word:
+        return True
     else:
-        print("CALC SELF!")
+        return False
+
+
+def cesarCode(txt, key=3):
+    with open("alfabet.json", 'r') as file:
+        alphabet_data = json.load(file)
+    code_in_number = []
+    for letter in txt:
+        for l in alphabet_data:
+            if alphabet_data[f"{l}"] == letter.lower():
+                code_in_number.append(int(l) + int(key))
+
+    code_word = ''
+    for num in code_in_number:
+        if num <= 26:
+            code_word += alphabet_data[f"{num}"]
+        else:
+            code_word += alphabet_data[f"{num - 26}"]
+
+    return code_word.capitalize()
+
+
+if __name__ == "__main__":
+    enter_txt = "Python"
+    print(enter_txt)
+    print(cesarCode(enter_txt))
