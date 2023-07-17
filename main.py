fbf51bab0a633606345f01c77e527c2f5641fe26
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-07-17 17:53:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import json
 
+
+def filterLetter(letter, data):
+    filtered_arr = []
+    for item in data:
+        title = item['title']
+        if title[0].upper() == letter:
+            filtered_arr.append(item)
+
+    return filtered_arr
+
+
 if __name__ == "__main__":
     with open("products.json", 'r', encoding="utf-8") as file:
         products = json.load(file)
 
+    print(filterLetter("F", products))
+    print(filterLetter("K", products))
+    print(filterLetter("Ш", products))
 
-    # temp_arr = []
-    # for item in products:
-    #     title = item['title']
-    #     print(title[0])
-    #     # if title[0] == "f":
-    #     #     temp_arr.append(item)
+    # new_arr = []
+    # for i in products:
+    #     if len(i['title']) != 0:
+    #         new_arr.append(i)
     #
-    # print(temp_arr)
-
-    new_arr = []
-    for i in products:
-        if len(i['title']) != 0:
-            new_arr.append(i)
-
-    with open("products.json", "w", encoding="utf-8") as f:
-        json.dump(new_arr, f)
-
+    # with open("products.json", "w", encoding="utf-8") as f:
+    #     json.dump(new_arr, f)
