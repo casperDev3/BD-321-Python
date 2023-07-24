4975a5a100490955d4d5ddd2a6a238c2d206d6fa
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-07-24 20:00:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
-import json
-import transform_temp as tt
+def fib(first_number, second_number, end_number):
+    fib_numbers = []
+    while second_number <= end_number:
+        next_number = first_number + second_number
+        first_number = second_number
+        second_number = next_number
+        if second_number <= end_number:
+            fib_numbers.append(next_number)
 
-
-def isPolindrom(word):
-    reverse_word = word[::-1]
-    if reverse_word == word:
-        return True
-    else:
-        return False
-
-
-def cesarCode(txt, key=3):
-    with open("alfabet.json", 'r') as file:
-        alphabet_data = json.load(file)
-    code_in_number = []
-    for letter in txt:
-        for l in alphabet_data:
-            if alphabet_data[f"{l}"] == letter.lower():
-                code_in_number.append(int(l) + int(key))
-
-    code_word = ''
-    for num in code_in_number:
-        if num <= 26:
-            code_word += alphabet_data[f"{num}"]
-        else:
-            code_word += alphabet_data[f"{num - 26}"]
-
-    return code_word.capitalize()
+    return fib_numbers
 
 
 if __name__ == "__main__":
-    enter_txt = "Python"
-    print(enter_txt)
-    print(cesarCode(enter_txt))
+    print(fib(1, 2, 100))
+
+
+
+
+
