ef5b9f05435ed4d8154a81c92643743a41faf6b8
Ihor Lialiuk <93039953+casperDev3@users.noreply.github.com>
2023-07-28 17:01:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,48 @@
-def fib(first_number, second_number, end_number):
-    fib_numbers = []
-    while second_number <= end_number:
-        next_number = first_number + second_number
-        first_number = second_number
-        second_number = next_number
-        if second_number <= end_number:
-            fib_numbers.append(next_number)
+def sumNumbers(numOne, numTwo):
+    sum = numOne + numTwo
+    return sum
 
-    return fib_numbers
+
+def createIntials(name, last_name, fathers_name):
+    initials = False
+    try:
+        initials = f"{last_name.capitalize()} {name[0].upper()}. {fathers_name[0].upper()}."
+    except Exception as err:
+        print("Сталась помилка в створенні ініціалів!")
+    return initials
+
+def runProgram():
+    sumNumbers(1, 2)
+    intitials = createIntials("Ihor", "Lialiuk", "Roman")
+    if intitials:
+        print(intitials)
+    temperature = int(input("Введіть температуру:"))
+    if temperature > 0:
+        print("Teplo")
+    else:
+        print("Cholodno")
 
 
 if __name__ == "__main__":
-    print(fib(1, 2, 100))
+    active = True
+    while active:
+        try:
+            runProgram()
+        except Exception as err:
+            print(err)
+        finally:
+            ch = input("Чи бажаєте продовжити користування (Y | N): ")
+            if ch.lower() == "n":
+                active = False
 
-
-
-
-
+    # try:
+    #     num = int("test")
+    #     print(num)
+    # except Exception as test:
+    #     try:
+    #         print(test)
+    #         print('Сталась помилка при розрахунку ціни')
+    #     except Exception as e:
+    #         print(e)
+    # finally:
+    #     print("Some text")
